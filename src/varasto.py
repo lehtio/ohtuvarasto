30417493a3bbe4fe8fbaf852126528844c106e07
lehtio <laura.l.lehtio@gmail.com>
2024-04-10 09:28:15 +0300

    hajotetaan testit t.9

--- src/varasto.py
+++ src/varasto.py
@@ -35,7 +35,7 @@ class Varasto:
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
 
-            return kaikki_mita_voidaan
+            return 1
 
         self.saldo = self.saldo - maara
 
